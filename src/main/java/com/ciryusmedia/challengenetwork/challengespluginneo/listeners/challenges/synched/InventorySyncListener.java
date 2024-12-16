12cbe7504adb7b5ca5ddb9dc6ab5a39b91aa5eeb
ImCIryus <nentonde@gmail.com>
2024-12-16 13:34:41 +0100

    Added a testable version of the inventory sync challenge

--- src/main/java/com/ciryusmedia/challengenetwork/challengespluginneo/listeners/challenges/synched/InventorySyncListener.java
+++ src/main/java/com/ciryusmedia/challengenetwork/challengespluginneo/listeners/challenges/synched/InventorySyncListener.java
@@ -1,41 +1,91 @@
 package com.ciryusmedia.challengenetwork.challengespluginneo.listeners.challenges.synched;
 
+import com.ciryusmedia.challengenetwork.challengespluginneo.ChallengesPluginNeo;
 import com.ciryusmedia.challengenetwork.challengespluginneo.challenges.Challenge;
+import com.ciryusmedia.challengenetwork.challengespluginneo.interfaces.Debuglevel;
 import com.ciryusmedia.challengenetwork.challengespluginneo.listeners.challenges.AChallengeListener;
+import io.papermc.paper.event.player.PlayerInventorySlotChangeEvent;
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityPickupItemEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryDragEvent;
+import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerItemDamageEvent;
 
 public class InventorySyncListener extends AChallengeListener implements Listener {
 
-    @EventHandler
-    public void onInventoryClick(InventoryClickEvent e) {
-        //TODO Player clicks on an item in the inventory
-    }
+    ChallengesPluginNeo instance = ChallengesPluginNeo.getInstance();
 
     @EventHandler
-    public void onInventoryDrag(InventoryDragEvent e) {
-        //TODO Player drags items in his inventory
+    public void onPlayerInventorySlotChange(PlayerInventorySlotChangeEvent e) {
+
+        if (!challenge.isEnabled() || !timer.isRunning()) {
+            return;
+        }
+
+        instance.log(e.getEventName(), Debuglevel.LEVEL_4);
+
+        Bukkit.getOnlinePlayers().forEach(player -> {
+            player.getInventory().setContents(e.getPlayer().getInventory().getContents());
+            player.getInventory().setArmorContents(e.getPlayer().getInventory().getArmorContents());
+        });
+
     }
 
-    @EventHandler
-    public void onPlayerItemDamage(PlayerItemDamageEvent e) {
-        //TODO Item gets damaged
-    }
-
-    @EventHandler
-    public void onPlayerItemDrop(PlayerDropItemEvent e) {
-        //TODO Player drops item
-    }
-
-    @EventHandler
-    public void onPlayerPickupItem(EntityPickupItemEvent e) {
-        //TODO Player picks up item
-    }
+//    @EventHandler
+//    public void onInventoryClick(InventoryClickEvent e) {
+//        //TODO Player clicks on an item in the inventory
+//
+//        if (e.getClickedInventory() == null
+//                || e.getCurrentItem() == null
+//                || e.getClickedInventory().getType() != InventoryType.PLAYER
+//                || !challenge.isEnabled()) {
+//            return;
+//        }
+//
+//        instance.log("Calling " + e.getEventName() + " for inventory sync", Debuglevel.LEVEL_4);
+//
+//    }
+//
+//    @EventHandler
+//    public void onInventoryDrag(InventoryDragEvent e) {
+//        //TODO Player drags items in his inventory
+//
+//        if (!challenge.isEnabled()) {
+//            return;
+//        }
+//
+//    }
+//
+//    @EventHandler
+//    public void onPlayerItemDamage(PlayerItemDamageEvent e) {
+//        //TODO Item gets damaged
+//
+//        if (!challenge.isEnabled()) {
+//            return;
+//        }
+//    }
+//
+//    @EventHandler
+//    public void onPlayerItemDrop(PlayerDropItemEvent e) {
+//        //TODO Player drops item
+//
+//        if (!challenge.isEnabled()) {
+//            return;
+//        }
+//    }
+//
+//    @EventHandler
+//    public void onPlayerPickupItem(EntityPickupItemEvent e) {
+//        //TODO Player picks up item
+//
+//        if (!challenge.isEnabled()) {
+//            return;
+//        }
+//    }
 
     public InventorySyncListener(Challenge challenge) {
         super(challenge);
