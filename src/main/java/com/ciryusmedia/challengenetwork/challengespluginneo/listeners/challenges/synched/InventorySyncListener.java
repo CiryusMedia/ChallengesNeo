30dbdf1dcb2a52a6f5e6350364b1739e7428e470
ImCIryus <nentonde@gmail.com>
2024-12-16 10:23:21 +0100

    Added the needed listeners for the Inventory Sync Challenge

--- src/main/java/com/ciryusmedia/challengenetwork/challengespluginneo/listeners/challenges/synched/InventorySyncListener.java
+++ src/main/java/com/ciryusmedia/challengenetwork/challengespluginneo/listeners/challenges/synched/InventorySyncListener.java
@@ -2,9 +2,40 @@ package com.ciryusmedia.challengenetwork.challengespluginneo.listeners.challenge
 
 import com.ciryusmedia.challengenetwork.challengespluginneo.challenges.Challenge;
 import com.ciryusmedia.challengenetwork.challengespluginneo.listeners.challenges.AChallengeListener;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityPickupItemEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryDragEvent;
+import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.event.player.PlayerItemDamageEvent;
 
 public class InventorySyncListener extends AChallengeListener implements Listener {
+
+    @EventHandler
+    public void onInventoryClick(InventoryClickEvent e) {
+        //TODO Player clicks on an item in the inventory
+    }
+
+    @EventHandler
+    public void onInventoryDrag(InventoryDragEvent e) {
+        //TODO Player drags items in his inventory
+    }
+
+    @EventHandler
+    public void onPlayerItemDamage(PlayerItemDamageEvent e) {
+        //TODO Item gets damaged
+    }
+
+    @EventHandler
+    public void onPlayerItemDrop(PlayerDropItemEvent e) {
+        //TODO Player drops item
+    }
+
+    @EventHandler
+    public void onPlayerPickupItem(EntityPickupItemEvent e) {
+        //TODO Player picks up item
+    }
 
     public InventorySyncListener(Challenge challenge) {
         super(challenge);
