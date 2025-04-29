f2d26e43fd91ba81d655ca9a97a1eb9bce49ba07
ImCiryus <nentonde@gmail.com>
2025-04-29 11:18:14 +0200

    Added the AGoal super contructor to the PlayerDeathListener

--- src/main/java/com/ciryusmedia/challengenetwork/challengespluginneo/gameplay/listeners/challenges/goal/PlayerDeathListener.java
+++ src/main/java/com/ciryusmedia/challengenetwork/challengespluginneo/gameplay/listeners/challenges/goal/PlayerDeathListener.java
@@ -1,10 +1,6 @@
 package com.ciryusmedia.challengenetwork.challengespluginneo.gameplay.listeners.challenges.goal;
 
-import com.ciryusmedia.challengenetwork.challengespluginneo.ChallengesPluginNeo;
-import com.ciryusmedia.challengenetwork.challengespluginneo.core.timer.ChallengeTimer;
-import com.ciryusmedia.challengenetwork.challengespluginneo.gameplay.challenges.Challenge;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -12,6 +8,10 @@ import org.bukkit.event.entity.PlayerDeathEvent;
 
 @SuppressWarnings({"deprecation"})
 public class PlayerDeathListener extends AGoal implements Listener {
+
+    public PlayerDeathListener() {
+        super(null);
+    }
 
     @EventHandler
     public void onPlayerDeath(PlayerDeathEvent event) {
