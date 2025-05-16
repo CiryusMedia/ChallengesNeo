e3ddaa2dffada2b0693ddf3272e5197b384d17ce
ImCiryus <nentonde@gmail.com>
2025-05-16 10:43:02 +0200

    Fixed an issue where the timer color guis would always have the paused color active

--- src/main/java/com/ciryusmedia/challengenetwork/challengespluginneo/gameplay/gui/GuiTimerColorItemStack.java
+++ src/main/java/com/ciryusmedia/challengenetwork/challengespluginneo/gameplay/gui/GuiTimerColorItemStack.java
@@ -3,8 +3,6 @@ package com.ciryusmedia.challengenetwork.challengespluginneo.gameplay.gui;
 import com.ciryusmedia.challengenetwork.challengespluginneo.ChallengesPluginNeo;
 import org.bukkit.Material;
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.List;
 
@@ -12,12 +10,11 @@ public class GuiTimerColorItemStack extends GuiItemStack {
 
     private static final FileConfiguration config = ChallengesPluginNeo.getChallengePlugin().getConfig();
 
-    private boolean running; //TODO Make this actually useful
     public final String key;
 
     public GuiTimerColorItemStack clone(boolean running) {
-        GuiTimerColorItemStack itemStack = new GuiTimerColorItemStack(this.getType(), this.getDisplayName(), this.getCustomModelData(), this.key, running);
-        itemStack.setEnchantmentGlint(isActiveColor(this.running));
+        GuiTimerColorItemStack itemStack = new GuiTimerColorItemStack(this.getType(), this.getDisplayName(), this.getCustomModelData(), this.key);
+        itemStack.setEnchantmentGlint(isActiveColor(running));
         return itemStack;
     }
 
@@ -29,22 +26,8 @@ public class GuiTimerColorItemStack extends GuiItemStack {
         }
     }
 
-    public void setRunning(boolean running) {
-        this.running = running;
-    }
-
-    public boolean isRunning() {
-        return this.running;
-    }
-
     public GuiTimerColorItemStack(Material material, String displayName, List<String> customModelData, String key) {
         super(material, displayName, customModelData);
         this.key = key;
-    }
-
-    public GuiTimerColorItemStack(Material material, String displayName, List<String> customModelData, String key, boolean running) {
-        super(material, displayName, customModelData);
-        this.key = key;
-        this.running = running;
     }
 }
