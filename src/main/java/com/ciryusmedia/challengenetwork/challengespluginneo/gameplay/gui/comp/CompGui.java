bbb08a6608ae94863f33d7c5fcbd6a84a33da411
Bastian <nentonde@gmail.com>
2025-09-25 19:58:38 +0200

    Fixed the CompGui not properly loading

--- src/main/java/com/ciryusmedia/challengenetwork/challengespluginneo/gameplay/gui/comp/CompGui.java
+++ src/main/java/com/ciryusmedia/challengenetwork/challengespluginneo/gameplay/gui/comp/CompGui.java
@@ -13,10 +13,14 @@ public class CompGui extends AGUIListener implements CompGuiItems, Commands {
 
     @Override
     public void inventoryClickHandler(ItemStack item, Player player) {
-        if (item.equals(Comp.COOP.item)) player.chat(COMP_CMD + " coop " + !Comp.COOP.isEnabled());
+        if (item.equals(Comp.COOP.item)) {
+            if (Comp.FFA.isEnabled()) player.chat(COMP_CMD + " " + Comp.FFA.key + " false");
+            player.chat(COMP_CMD + " coop " + !Comp.COOP.isEnabled());
+        }
         if (item.equals(Comp.FFA.item)) {
-            player.chat(GOALS_CMD + " " + Goal.PLAYER_DEATH.key + " false");
-            player.chat(COMP_CMD + " ffa " + !Comp.FFA.isEnabled());
+            if (Goal.PLAYER_DEATH.isEnabled()) player.chat(GOALS_CMD + " " + Goal.PLAYER_DEATH.key + " false");
+            if (Comp.COOP.isEnabled()) player.chat(COMP_CMD + " " + Comp.COOP.key + " false");
+            player.chat(COMP_CMD + " " + Comp.FFA.key + " " + !Comp.FFA.isEnabled());
         }
 //        if (item.equals(Coop.TEAMS.item)) player.chat(COMP_CMD + " teams " + !Coop.TEAMS.isEnabled());
     }
@@ -24,15 +28,16 @@ public class CompGui extends AGUIListener implements CompGuiItems, Commands {
     @Override
     public void initInv() {
         inv = Bukkit.createInventory(null, 3 * 9, "Comp");
-
-        inv.setItem(10, Comp.COOP.item);
-        inv.setItem(12, Comp.FFA.item);
-//        inv.setItem(14, Comp.TEAMS.item);
+        updateInventory();
     }
 
     @Override
     public void updateInventory() {
         emptyInventoryItemFiller(inv, fillerItem, lineFillerItem);
+
+        inv.setItem(10, Comp.COOP.item);
+        inv.setItem(12, Comp.FFA.item);
+//        inv.setItem(14, Comp.TEAMS.item);
     }
 
     public CompGui() {
