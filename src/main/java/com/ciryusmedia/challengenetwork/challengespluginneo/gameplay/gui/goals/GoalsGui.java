389aa1708b2cfe9fc8449491428a59ae1a83c417
Bastian <nentonde@gmail.com>
2025-09-25 19:59:14 +0200

    Reduced the amount of commands sent when enabling or disabling the player death failure goal

--- src/main/java/com/ciryusmedia/challengenetwork/challengespluginneo/gameplay/gui/goals/GoalsGui.java
+++ src/main/java/com/ciryusmedia/challengenetwork/challengespluginneo/gameplay/gui/goals/GoalsGui.java
@@ -2,6 +2,7 @@ package com.ciryusmedia.challengenetwork.challengespluginneo.gameplay.gui.goals;
 
 import com.ciryusmedia.challengenetwork.challengespluginneo.core.console.ChallengeLogger;
 import com.ciryusmedia.challengenetwork.challengespluginneo.gameplay.commands.Commands;
+import com.ciryusmedia.challengenetwork.challengespluginneo.gameplay.comp.Comp;
 import com.ciryusmedia.challengenetwork.challengespluginneo.gameplay.goals.Goal;
 import com.ciryusmedia.challengenetwork.challengespluginneo.gameplay.goals.GoalType;
 import com.ciryusmedia.challengenetwork.challengespluginneo.gameplay.gui.AGUIListener;
@@ -17,8 +18,10 @@ public class GoalsGui extends AGUIListener implements GoalsGuiItems, Commands {
         if (item.equals(Goal.KILL_ENDER_DRAGON.item)) handleGoal(Goal.KILL_ENDER_DRAGON, player); //TODO do it with !goal.isEnabled
         else if (item.equals(Goal.GET_ALL_ADVANCEMENTS.item)) handleGoal(Goal.GET_ALL_ADVANCEMENTS, player);
         else if (item.equals(Goal.PLAYER_DEATH.item)) {
-            player.chat(COMP_CMD + " ffa false");
-            player.chat(COMP_CMD + " coop true");
+            if (Comp.FFA.isEnabled()) {
+                player.chat(COMP_CMD + " ffa false");
+                player.chat(COMP_CMD + " coop true");
+            }
             handleGoal(Goal.PLAYER_DEATH, player);
         }
 
